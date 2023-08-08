7984be231f32dac346e4f8fc483092ced8aecd79
TomekRogal <tomekrogalski@vp.pl>
2023-08-08 20:50:29 +0200

    formularz

--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -3,12 +3,16 @@ package pl.coderslab.charity.donation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.category.Category;
 import pl.coderslab.charity.category.CategoryRepository;
+import pl.coderslab.charity.institution.Institution;
 import pl.coderslab.charity.institution.InstitutionRepository;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class DonationController {
@@ -21,12 +25,20 @@ public class DonationController {
         this.institutionRepository = institutionRepository;
         this.donationRepository = donationRepository;
     }
+    @ModelAttribute("categories")
+    public List<Category> categories() {
+        return categoryRepository.findAll();
+    }
+    @ModelAttribute("institutions")
+    public List<Institution> institutions() {
+        return institutionRepository.findAll();
+    }
 
     @RequestMapping("/donation")
     public String add(Model model) {
         Donation donation = new Donation();
-        model.addAttribute("categories",categoryRepository.findAll());
-        model.addAttribute("institutions",institutionRepository.findAll());
+//        model.addAttribute("categories",categoryRepository.findAll());
+//        model.addAttribute("institutions",institutionRepository.findAll());
         model.addAttribute("donation",donation);
         return "form";
     }
