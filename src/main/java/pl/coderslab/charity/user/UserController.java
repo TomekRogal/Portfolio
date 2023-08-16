a0e08d4234edf68cd2e2e6084035e61a6220cdbf
TomekRogal <tomekrogalski@vp.pl>
2023-08-16 19:56:34 +0200

    token

--- src/main/java/pl/coderslab/charity/user/UserController.java
+++ src/main/java/pl/coderslab/charity/user/UserController.java
@@ -6,12 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.charity.email.MailService;
-import pl.coderslab.charity.role.Role;
 import pl.coderslab.charity.role.RoleRepository;
 
 import javax.mail.MessagingException;
 import javax.validation.Valid;
-import java.util.Set;
 
 
 @SessionAttributes("loggedUser")
